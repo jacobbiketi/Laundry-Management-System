b719ad35be941e1c387e7824c55ec9515658e606
jacobbiketi <jacobbiketi2023@gmail.com>
2024-04-05 12:22:17 +0300

    j

--- registration .php
+++ registration .php
@@ -2,33 +2,27 @@
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the required keys exist in the $_POST array
-    if (isset($_POST['name'], $_POST['email'], $_POST['phone_no'], $_POST['password'], $_POST['confirm-password'])) {
+    if (isset($_POST['name'], $_POST['email'], $_POST['phone_no'], $_POST['password'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $phone_no = $_POST['phone_no'];
         $password = $_POST['password'];
-        $confirm_password = $_POST['confirm-password'];
 
-        // Check if password matches confirm password
-        if ($password === $confirm_password) {
-            $link = mysqli_connect('localhost', 'root', '', 'graduation');
-            if ($link === false) {
-                die("Error: Unable to connect " . mysqli_connect_error());
-            } else {
-                $stmt = $link->prepare("INSERT INTO customers (name, email, phone_no, password) VALUES (?, ?, ?, ?)");
-                $stmt->bind_param("ssss", $name, $email, $phone_no, $password);
-
-                if ($stmt->execute()) {
-                    echo "Registration successful!";
-                } else {
-                    echo "Error: " . $stmt->error;
-                }
-
-                $stmt->close();
-                $link->close();
-            }
+        $link = mysqli_connect('localhost', 'root', '', 'lms');
+        if ($link === false) {
+            die("Error: Unable to connect " . mysqli_connect_error());
         } else {
-            echo "Error: Password does not match the confirm password.";
+            $stmt = $link->prepare("INSERT INTO customers (name, email, phone_no, password) VALUES ( ?,  ?, ?, ?)");
+            $stmt->bind_param("sssss", $name, $email, $phone_no, $password);
+
+            if ($stmt->execute()) {
+                echo "Registration successful!";
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+
+            $stmt->close();
+            $link->close();
         }
     } else {
         echo "Error: Required fields are missing.";
